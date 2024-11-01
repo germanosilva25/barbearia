829351758597c4362f1a1aa5140e97fef82fff86
germanosilva25 <germanosilva@yahoo.com.br>
2024-11-01 14:36:33 -0300

    insert agenda

--- Agenda/criar_agenda.php
+++ Agenda/criar_agenda.php
@@ -1,11 +1,12 @@
 <?php
-echo "<pre>";
-print_r($_POST);
+include "../connection.php";
+
 
 $inicio = strtotime($_POST['horario_inicio']);
 $final = strtotime($_POST['horario_saida_intervelo']);
+$dia_da_semana = $_POST['dia_da_semana'];
+$id_usuario = $_POST['id_usuario'];
 $mins = ($inicio - $final) / 60;
-
 if($mins < 0)
     $mins = $mins*(-1);
 //echo $mins;
@@ -19,10 +20,21 @@ echo "<h1>Geração de agendamentos do primeiro horário!</h1>";
 for($i = 0; $i < $qnt_de_agendamento; $i++){
 
     echo "\n Criado agendamento no horario para: " . date('H:i', $acumulado);
+    $horario = date('H:i', $acumulado);
+    incluirAgenda($dia_da_semana, $id_usuario, $horario, $conn);
     $acumulado = $acumulado + 1800;
 }
 
+function incluirAgenda($dia_da_semana, $id_usuario, $horario, $conn){
+    $sql_agenda = "INSERT INTO agendas (dia_da_semana , id_usuario, horario)
+    VALUES ($dia_da_semana, $id_usuario, '$horario')";
 
+    if ($conn->query($sql_agenda) === TRUE) {
+    echo "New record created successfully";
+    } else {
+    echo "Error: " . $sql_agenda . "<br>" . $conn->error;
+    }
+}
 
 
 $inicio = strtotime($_POST['horario_volta_intervela']);
